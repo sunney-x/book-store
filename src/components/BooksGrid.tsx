c41efa76c6764ab6fa8d4b73033a3223d1b8664f
sunney-x <sanishubitidze@live.com>
2022-05-12 23:26:48 +0400

    centered items on grid

--- src/components/BooksGrid.tsx
+++ src/components/BooksGrid.tsx
@@ -8,7 +8,7 @@ const BooksGrid = (props: {
 	const { books } = props;
 
 	return (
-		<div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-9 p-9">
+		<div className="grid justify-items-center sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-9 p-9">
 			{books.map((book) => (
 				<Link href={`/book/${book.id}`} key={book.id} passHref>
 					<BookListing key={book.id} book={book} />
